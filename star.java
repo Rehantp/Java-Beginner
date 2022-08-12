076640c714a40749243ee553f6b54602022a2ce4
Rehan Perera <95935466+Rehantp@users.noreply.github.com>
2022-08-12 22:18:21 +0530

    lab 1  square *

---
+++ star.java
@@ -0,0 +1,24 @@
+
+/*  * * * * *
+    * * * * *
+    * * * * *
+    * * * * *
+    * * * * *    */
+public class star{
+	
+	public static void main(String[] args)
+	{
+		for(int i=1 ; i<=5 ; i++)
+		{
+			for(int l=1 ; l<=5 ; l++)
+			{	
+			   System.out.print("* ");
+			}
+			 System.out.print("\n");
+		}
+	}
+	
+	
+	
+
+}
